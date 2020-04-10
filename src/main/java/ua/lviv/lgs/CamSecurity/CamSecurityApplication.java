4d2b3f43643c851ae9d0ab9360c2217b0ab68566
MJava19 <56982380+MJava19@users.noreply.github.com>
2020-04-10 13:18:58 +0300

    First commit

---
+++ src/main/java/ua/lviv/lgs/CamSecurity/CamSecurityApplication.java
@@ -0,0 +1,13 @@
+package ua.lviv.lgs.CamSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/lviv/lgs/CamSecurity/CamSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.lviv.lgs.CamSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
